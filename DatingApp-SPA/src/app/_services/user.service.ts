a370f2b44fba8e8f1a2424b9ff87e53073d9593f
Karen Rodrigues <derik.ferrao@gmail.com>
2020-01-19 00:39:38 +0000

    End of Section 10

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -19,4 +19,8 @@ export class UserService {
   getUser(id): Observable<User> {
     return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
+
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + 'users/' + user.id, user);   //this might be wrong u have added it as user.id, should be id was giving undefined
+  }
 }
--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -5,11 +5,14 @@ using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System;
 
 namespace DatingApp.API.Controllers
 {
     [Authorize]
     [Route("api/[controller]")]
+    [ApiController]
     public class UsersController : ControllerBase
     {
         private readonly IDatingRepository _repo;
@@ -34,6 +37,24 @@ namespace DatingApp.API.Controllers
             var user = await _repo.GetUser(id);
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            
+                if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                    return Unauthorized();
+
+                var userFromRepo = await _repo.GetUser(id);
+
+                _mapper.Map(userForUpdateDto, userFromRepo);
+
+                if (await _repo.SaveAll())
+                    return NoContent();
+                
+                throw new Exception($"Updating user {id} failed on save");
+            
         }
     }
 }
